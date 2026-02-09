2d02e814df238500db81783cc2042664f72c1e2b
Matt B <cronos1979@live.com>
2026-02-08 21:48:19 -0600

    Integrate SQLite amalgamation and fix UE 5.7 compile warnings

--- Source/AstralFrontier/AstralFrontier.Build.cs
+++ Source/AstralFrontier/AstralFrontier.Build.cs
@@ -1,23 +1,41 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class AstralFrontier : ModuleRules
 {
 	public AstralFrontier(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        CppCompileWarningSettings.UndefinedIdentifierWarningLevel = WarningLevel.Off;
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "NetCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		string SQLitePath = Path.Combine(ModuleDirectory, "ThirdParty", "SQLite");
+
+		PublicIncludePaths.Add(SQLitePath);
+
+		// Ensure sqlite3.c is compiled
+		PrivateDefinitions.Add("SQLITE_THREADSAFE=1");
+		PrivateDefinitions.Add("SQLITE_ENABLE_FTS5=1");
+		PrivateDefinitions.Add("SQLITE_ENABLE_JSON1=1");
+
+        PrivateDefinitions.Add("SQLITE_OS_WIN=1");
+        PrivateDefinitions.Add("SQLITE_OS_UNIX=0");
+        PrivateDefinitions.Add("SQLITE_OS_OTHER=0");
+        PrivateDefinitions.Add("SQLITE_OS_KV=0");
+
+
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
---
+++ Source/AstralFrontier/Core/AFRUniverseGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AFRUniverseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTRALFRONTIER_API AAFRUniverseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
